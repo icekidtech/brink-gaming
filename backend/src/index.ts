a54d39104728f0424de2b0bd1e0cd36ebc546474
icekidtech <princeklvn7568@gmail.com>
2025-03-05 02:19:47 +0100

    `Added user registration route and initialized it in main app file`

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,14 @@ import { AppDataSource } from "./config/db";
 import express from "express";
 import cors from "cors";
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api", userRoutes)
 
 // Database Initialization
 AppDataSource.initialize()
